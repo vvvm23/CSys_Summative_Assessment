d529a662af97b92af16898f8429c31c3d6053d25
Alexander McKinney <alexander.f.mckinney@durham.ac.uk>
2018-10-26 11:04:59 +0100

    Python demo of conversion between bases.

---
+++ BaseConvertor.py
@@ -0,0 +1,32 @@
+def get_digits(integer):
+    a, integer = int_div(integer, 100)
+    b, integer = int_div(integer, 10)
+    c = integer
+
+    return a,b,c
+
+def int_div(integer, div):
+    count = 0
+    while integer - div >= 0:
+        integer -= div
+        count += 1
+
+    return count, integer
+
+def convert_base10_to_x(integer, base):
+    a, integer = int_div(integer, base**2)
+    b, integer = int_div(integer, base)
+    c = integer
+
+    if a >= base:
+        return 999
+    else:
+        return a*100 + b*10 + c
+
+def convert_x_to_base10(integer, base):
+    a,b,c = get_digits(integer)
+    return a*base**2 + b*base + c
+
+x = 213; print(x)
+x = convert_base10_to_x(x, 9); print(x)
+x = convert_x_to_base10(x, 9); print(x)
